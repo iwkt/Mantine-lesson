6fd50bc3fae589aad0c7fb625c312117e71157ea
iwkt <iwk@mac.com>
2022-07-15 06:31:06 +0900

    button

--- pages/button.tsx
+++ pages/button.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import { Button, Group } from "@mantine/core";
 import { Layout } from "../components/Layout";
+import { BrandGithub, ThreeDCubeSphere } from "tabler-icons-react";
 
 const ButtonDemo = () => {
-  return <Layout title="Button">button</Layout>;
+  return (
+    <Layout title="Button">
+      <Group position="center">
+        <Button
+          color="cyan"
+          radius="md"
+          size="xl"
+          uppercase
+          leftIcon={<BrandGithub />}
+        >
+          Press
+        </Button>
+      </Group>
+    </Layout>
+  );
 };
 
 export default ButtonDemo;
